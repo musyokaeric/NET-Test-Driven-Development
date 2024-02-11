cd2cc0a94a5bb2b3529bae08a5556df955a5f0d8
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-02-11 08:14:33 +0300

    Created Unit Test Based on Requirement

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessor.cs
@@ -0,0 +1,20 @@
+﻿
+namespace RoomBookingApp.Core
+{
+    internal class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        internal RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
+        {
+            return new RoomBookingResult
+            {
+                FullName = bookingRequest.FullName,
+                Email = bookingRequest.Email,
+                Date = bookingRequest.Date,
+            };
+        }
+    }
+}
