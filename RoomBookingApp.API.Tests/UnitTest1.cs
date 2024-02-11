ed19a8e50369f5a7fe8064b61ef04809c748a803
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-02-11 13:54:53 +0300

    Testing Simple .NET Core Web API

---
+++ RoomBookingApp.API.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using RoomBookingApp.API.Controllers;
+
+namespace RoomBookingApp.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Should_Return_Forecast_Results()
+        {
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            var result = controller.Get();
+
+            Assert.NotNull(result);
+            result.Count().ShouldBeGreaterThan(1);
+        }
+    }
+}
